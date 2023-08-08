12c20f895d71aba93ec1a2d921096e7b16c14b57
Prabeernandi <prabeerveer77@gmail.com>
2023-08-08 12:24:47 +0530

    Added IP Address Feature

---
+++ src/main/java/com/anganwadi/anganwadi/config/LoggerInterceptor.java
@@ -0,0 +1,20 @@
+package com.anganwadi.anganwadi.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@Slf4j
+public class LoggerInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
+
+      log.error("IP :"+request.getRemoteAddr());
+      return true;
+    }
+}
